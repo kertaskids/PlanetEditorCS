c001bbc029df48dbc9fa29a182bdc3f43ad495ca
kertaskids <kertaskids@gmail.com>
2017-01-22 11:46:04 +0800

    Uploading
    Uploading files

---
+++ PlanetEditorCS/main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlanetEditorCS
+{
+    class main
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello!");
+            Console.ReadKey();
+        }
+    }
+
+    class Lion : CreatureType { 
+    
+    }
+
+    class Plant : CreatureType { 
+        
+    } 
+}
